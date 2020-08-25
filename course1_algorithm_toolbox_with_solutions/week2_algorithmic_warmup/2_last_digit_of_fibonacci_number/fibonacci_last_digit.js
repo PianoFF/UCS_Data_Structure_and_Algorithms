2e539f26669c18591e0a027c6f744e733c3b47f0
pianoFF <fangfei1star@gmail.com>
2020-08-25 18:19:04 -0500

    last digit of a large fibonacci number

--- course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
+++ course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
@@ -1,21 +1,34 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    // write your code here
+  // write your code here
+  let result = 1;
+  let nMinus1 = 1;
+  let nMinus2 = 0;
+  let count = 2;
+
+  while (count <= n) {
+    result = nMinus1 + nMinus2;
+    result = result % 10;
+    count += 1;
+    nMinus2 = nMinus1;
+    nMinus1 = result;
+  }
+  return result;
 }
 
 module.exports = fib;
