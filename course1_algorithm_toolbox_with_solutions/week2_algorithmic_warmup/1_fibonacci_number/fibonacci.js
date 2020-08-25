e861ce6e58a7d3d4a68382282f228bc700f0ff29
pianoFF <fangfei1star@gmail.com>
2020-08-24 21:00:20 -0500

    fib number

--- course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
+++ course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
@@ -1,21 +1,37 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    // write your code here
+  // write your code here
+  if (n === 0) {
+    return 0;
+  } else {
+    let result = 0;
+    let count = 2;
+    let nMinus2 = 0;
+    let nMinus1 = 1;
+    result = nMinus2 + nMinus1;
+    while (count <= n) {
+      result = nMinus2 + nMinus1;
+      nMinus2 = nMinus1;
+      nMinus1 = result;
+      count += 1;
+    }
+    return result;
+  }
 }
 
 module.exports = fib;
