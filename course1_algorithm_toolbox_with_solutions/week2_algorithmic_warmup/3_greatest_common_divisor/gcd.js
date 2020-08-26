4627fd30f329df228e8f1b9e2e665a30c201bb1d
pianoFF <fangfei1star@gmail.com>
2020-08-25 19:37:10 -0500

    Greatest Common Divisor

--- course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.js
+++ course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.js
@@ -1,26 +1,49 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    if (line !== "\n") {
-        const a = parseInt(line.toString().split(' ')[0], 10);
-        const b = parseInt(line.toString().split(' ')[1], 10);
+  if (line !== "\n") {
+    const a = parseInt(line.toString().split(" ")[0], 10);
+    const b = parseInt(line.toString().split(" ")[1], 10);
 
-        console.log(gcd(a, b));
-        process.exit();
-    }
+    console.log(gcd(a, b));
+    process.exit();
+  }
 }
 
 function gcd(a, b) {
-    // write your code here
+  // write your code here
+  let remainder = a;
+
+  let greaterItem;
+  let smallerItem;
+
+  if (a > b) {
+    greaterItem = a;
+    smallerItem = b;
+  } else {
+    greaterItem = b;
+    smallerItem = a;
+  }
+
+  while (remainder > 0) {
+    remainder = greaterItem % smallerItem;
+    // console.log(remainder);
+    if (remainder === 0) {
+      return smallerItem;
+    } else {
+      greaterItem = smallerItem;
+      smallerItem = remainder;
+    }
+  }
 }
 
 module.exports = gcd;
