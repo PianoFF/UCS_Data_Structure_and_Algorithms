fe361e47ca5512d84bf4edbf9978947c07691540
pianoFF <fangfei1star@gmail.com>
2020-08-25 22:29:25 -0500

    Fib mod m_Pisano period

--- course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.js
+++ course1_algorithm_toolbox_with_solutions/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.js
@@ -1,27 +1,49 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    if (line !== "\n") {
-        const n = parseInt(line.toString().split(' ')[0], 10);
-        const m = parseInt(line.toString().split(' ')[1], 10);
+  if (line !== "\n") {
+    const n = parseInt(line.toString().split(" ")[0], 10);
+    const m = parseInt(line.toString().split(" ")[1], 10);
 
-        console.log(getFibMod(n, m));
-        process.exit();
-    }
+    console.log(getFibMod(n, m));
+    process.exit();
+  }
 }
 
-
 function getFibMod(n, m) {
-    // write your code here
+  // write your code here
+  let pisanoPeriod = [0, 1, 1];
+  let result = 0;
+  let nMinus2 = 1;
+  let nMinus1 = 1;
+
+  while (
+    pisanoPeriod[pisanoPeriod.length - 1] != 1 ||
+    pisanoPeriod[pisanoPeriod.length - 2] != 0
+  ) {
+    result = nMinus2 + nMinus1;
+    pisanoPeriod.push(result % m);
+    // console.log("pisano Period:", pisanoPeriod);
+    nMinus2 = pisanoPeriod[pisanoPeriod.length - 2];
+    nMinus1 = pisanoPeriod[pisanoPeriod.length - 1];
+  }
+  //   console.log("before cutting", pisanoPeriod);
+  pisanoPeriod.pop();
+  pisanoPeriod.pop();
+
+  //   console.log("after trimming", pisanoPeriod);
+
+  let index = n % pisanoPeriod.length;
+  return pisanoPeriod[index];
 }
 
 module.exports = getFibMod;
